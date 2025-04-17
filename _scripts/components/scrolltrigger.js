ccc6ea348a6a84062a4598f867f90d4a2fb92386
Harry Pujols <harrypujols@gmail.com>
2025-04-17 19:39:31 +0200

    changing observer

--- _scripts/components/scrolltrigger.js
+++ _scripts/components/scrolltrigger.js
@@ -1,18 +1,23 @@
 export default class {
   constructor(element, APP) {
     this.element = element;
+    this.entries = this.element.querySelectorAll(".entry");
     this.scroll = APP.methods.scrollstop;
   }
 
   isInViewport() {
-    const rect = this.element.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+    const observer = new IntersectionObserver((this.entries) => {
+      this.entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("is-in-viewport");
+          // observer.unobserve(entry.target);
+          return true
+        } else {
+          entry.target.classList.remove("is-in-viewport");
+          return false
+        }
+      });
+    }
   }
 
   init() {
