befd1fc14de459ee86e3800cc524bd322e1f2bcc
Harry Pujols <harrypujols@gmail.com>
2025-05-09 12:31:35 -0400

    Retooling nav for hamburger

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -447,45 +447,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (class {
   constructor(element, APP) {
     this.element = element;
-    this.scroll = APP.methods.scrollstop;
-    this.isInViewport = APP.methods.isInViewport;
-    this.links = this.element.querySelectorAll(".js-nav-link");
-    this.intro = document.querySelector("#intro");
-  }
-
-  async makeActive() {
-    for (const link of this.links) {
-      const target = document.querySelector(link.getAttribute("href"));
-      if (target) {
-        const isVisible = await this.isInViewport(target);
-        if (isVisible) {
-          link.classList.add("is-active");
-        } else {
-          link.classList.remove("is-active");
-        }
-      }
-    }
-  }
-
-  async isVisible() {
-    if (this.intro) {
-      const isVisible = await this.isInViewport(this.intro);
-      if (!isVisible) {
-        this.element.classList.add("is-visible");
-      } else {
-        this.element.classList.remove("is-visible");
-      }
-    }
   }
 
   init() {
-    this.makeActive();
-    this.isVisible();
-
-    this.scroll(() => {
-      this.makeActive();
-      this.isVisible();
-    }, 15);
+    console.log("Navigation component initialized");
   }
 });
 
