f09bf4d229ee895cb7b07b856384d7faddb8c4df
Harry Pujols <harrypujols@gmail.com>
2025-05-12 14:10:41 -0400

    functionality of hamburger done

--- _scripts/components/navigation.js
+++ _scripts/components/navigation.js
@@ -1,11 +1,27 @@
 export default class {
   constructor(element, APP) {
     this.element = element;
-    this.openButton = this.element.querySelector("js-hamburger-button");
-    this.menu = this.element.querySelector("js-navigation-menu");
+    this.openButton = this.element.querySelector(".js-hamburger-button");
+    this.menu = this.element.querySelector(".js-navigation-menu");
+  }
+
+  toggle() {
+    this.element.addEventListener("click", (event) => {
+      console.log(event.target);
+      if (event.target.closest(".js-hamburger-button")) {
+        event.preventDefault();
+        console.log("Hamburger button clicked");
+        this.menu.classList.toggle("is-open");
+        this.menu.setAttribute(
+          "aria-expanded",
+          !this.menu.classList.contains("is-open")
+        );
+      }
+    });
   }
 
   init() {
     console.log("Navigation component initialized");
+    this.toggle();
   }
 }
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -447,12 +447,28 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (class {
   constructor(element, APP) {
     this.element = element;
-    this.openButton = this.element.querySelector("js-hamburger-button");
-    this.menu = this.element.querySelector("js-navigation-menu");
+    this.openButton = this.element.querySelector(".js-hamburger-button");
+    this.menu = this.element.querySelector(".js-navigation-menu");
+  }
+
+  toggle() {
+    this.element.addEventListener("click", (event) => {
+      console.log(event.target);
+      if (event.target.closest(".js-hamburger-button")) {
+        event.preventDefault();
+        console.log("Hamburger button clicked");
+        this.menu.classList.toggle("is-open");
+        this.menu.setAttribute(
+          "aria-expanded",
+          !this.menu.classList.contains("is-open")
+        );
+      }
+    });
   }
 
   init() {
     console.log("Navigation component initialized");
+    this.toggle();
   }
 });
 
